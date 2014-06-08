7f7b5ab8fcec3898573e29f2a3ec20b728b2a9b6
bdone2012 <bdone2012@gmail.com>
2014-06-08 16:51:12 -0400

    got toggle menu working

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,52 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+// $('nav li').click(toggleMenu);
+
+// function toggleMenu() {
+
+// }
+$(document).ready(function() {
+
+
+  $("#writer").hide();
+  $( '#writer_name').click(function(){
+  $("#writer").toggle();
+  $("#writer").siblings().hide();
+  });
+
+  $("#director").hide();
+  $( '#director_name').click(function(){
+  $("#director").toggle();
+  $("#director").siblings().hide();
+  });
+
+  $("#dp").hide();
+  $( '#dp_name').click(function(){
+  $("#dp").toggle();
+  $("#dp").siblings().hide();
+  });
+
+  $("#boom").hide();
+  $( '#boom_name').click(function(){
+  $("#boom").toggle();
+  $("#boom").siblings().hide();
+  });
+
+  $("#ad").hide();
+  $( '#ad_name').click(function(){
+  $("#ad").toggle();
+  $("#ad").siblings().hide();
+  });
+
+  $("#pa").hide();
+  $( '#pa_name').click(function(){
+  $("#pa").toggle();
+  $("#pa").siblings().hide();
+  });
+
+ });
+
+
+
